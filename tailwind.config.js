fae9e488979b212dc351b1ab57fcc3f64bd6ae0a
charlene-chenn <qcharlene8@gmail.com>
2024-10-01 10:49:07 +0100

    Update changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
