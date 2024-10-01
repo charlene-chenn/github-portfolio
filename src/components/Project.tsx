a92975e6293a33e954b5c94775cfec01df573e1a
charlene-chenn <qcharlene8@gmail.com>
2024-10-01 10:50:18 +0100

    Update changes

---
+++ src/components/Project.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useState } from 'react';
+import { FaGithub } from 'react-icons/fa'
+import { FaArrowDownWideShort } from "react-icons/fa6";
+import { FaArrowUpWideShort } from "react-icons/fa6";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropup } from "react-icons/io";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { RiArrowUpSLine } from "react-icons/ri";
+
+interface Props {
+    title: string;
+    description: string;
+    media: string;
+    tags: string[];
+}
+
+function Project({ title, description, media, tags} : Props) {
+    const [isDescriptionVisible, setDescriptionVisible] = useState(false);
+    const toggleDescription = () => {
+        setDescriptionVisible(!isDescriptionVisible);
+    }
+
+    return (
+        <div className="project-tiles">
+            <div className="project-tile project-icons">
+                <img src={media} alt={title} className="project-image" />
+                <div className="project-overlay absolute inset-0 flex opacity-0 hover:opacity-100 font-bold">
+                    <h3 className="project-title text-left">{title}</h3>
+                    <div className="project-tags flex flex-wrap">
+                        {tags.map((tag, index) => (
+                            <span key={index} className="tag">{tag}</span>
+                        ))}
+                    </div>
+                    <hr className="w-11/12 my-2 divider" />
+                    <div className={`project-description text-left ${isDescriptionVisible 
+                        ? 'visible' : 'hidden'}`} style={{ marginTop: isDescriptionVisible ? '0px' : '0', opacity: isDescriptionVisible ? '1' : '0', transition: 'opacity 0.5s ease-out, margin-top 0.5s ease-out'}}>
+                        {description}
+                    </div>
+                    <button onClick={toggleDescription} className="read-more-button mt-2 flex items-center">
+                        {isDescriptionVisible ? (
+                            <>
+                                <span className="mr-2">read less</span>
+                                <RiArrowUpSLine />
+                            </>
+                        ) : (
+                            <>
+                                <span className="mr-2">read more</span>
+                                <RiArrowDownSLine />
+                            </>
+                        )}
+                    </button>
+                
+                </div>
+                {/* <h5 className="project-title">{title}</h5> */}
+                {/* <p className="project-description">Description of Project 1.</p> */}
+                {/* <a href="project1.html" className="project-link">Read More</a> */}
+            </div>
+        </div>
+
+    );
+};
+
+export default Project;
