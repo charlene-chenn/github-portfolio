36321e7f039aac86049469fc59aa75d468d2c824
Charlene Chen <57976532+charlene-chenn@users.noreply.github.com>
2024-11-01 21:26:02 +0000

    Update vite.config.ts
    Fix conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',  // Specify the output directory as 'build' instead of the default 'dist'
   },
   base: '/github-portfolio/',
-=======
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 36fecb9 (Update changes)
-})
