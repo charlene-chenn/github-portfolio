4b2b4a639421c0a1a9709ccfff02f6ef714a9ca5
Charlene Chen <57976532+charlene-chenn@users.noreply.github.com>
2024-11-02 11:16:25 +0000

    Update main.tsx
    Remove conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-<<<<<<< HEAD
 import "./input.css"
-=======
-import './index.css'
->>>>>>> 36fecb9 (Update changes)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
