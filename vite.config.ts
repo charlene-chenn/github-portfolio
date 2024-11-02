3afba8d91d76b472959226828fbcc729e133c332
Charlene Chen <57976532+charlene-chenn@users.noreply.github.com>
2024-11-02 10:36:56 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './github-portfolio/',
+  base: '/github-portfolio/',
   plugins: [react()]
   build: {
     outDir: 'build',  // Specify the output directory as 'build' instead of the default 'dist'
